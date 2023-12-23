1542b3203a2210339457ef5deea3df37cb45572e
santhosh <santhosh10661@gmail.com>
2023-12-23 14:35:52 +0530

    after routes changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Services from "./Services";
 import About from "./About";
 import ContactUs from "./Contact us";
 
 function App() {
+  const navigate = useNavigate();
+
+  // Redirect to the desired default route ("/content" in this case)
+  useEffect(() => {
+    navigate("/", { replace: true });
+  }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" exact element={<Content />} />
+        {/* Default route for the home page */}
+        <Route path="/" element={<Content />} />
+        {/* Other routes */}
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactUs />} />
